e04f8cd79b6367942d9fca8c2f21319f5d920625
Imtiaz <im3azchowdhury@gmail.com>
2023-11-08 23:47:46 +0600

    single food data fetched from database

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,7 +11,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.thriitw.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -22,15 +21,40 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const availableFoodsCollection = client
+      .db("shareBite")
+      .collection("availableFoods");
+
+    app.get("/availableFoods", async (req, res) => {
+      // console.log(req.query.foodStatus);
+      const query = { foodStatus: req.query.foodStatus };
+      const cursor = availableFoodsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/availableFoods/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await availableFoodsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/availableFoods", async (req, res) => {
+      const food = req.body;
+      const result = await availableFoodsCollection.insertOne(food);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
